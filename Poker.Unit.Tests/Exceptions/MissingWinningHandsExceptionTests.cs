375a2bca3f2f939dc257ac83c410a40ff4122d16
Victor De Velasco <victor91dv@hotmail.com>
2020-07-27 02:38:08 -0700

    Added Missing Winning Hands Exception.

---
+++ Poker.Unit.Tests/Exceptions/MissingWinningHandsExceptionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Poker.Exceptions;
+using Poker.Unit.Tests.Exceptions.Base;
+
+namespace Poker.Unit.Tests.Exceptions
+{
+    [TestFixture]
+    public class MissingWinningHandsExceptionTests : BaseExceptionTests
+    {
+        [Test]
+        public void ShouldInstantiateConstructorWithParameters()
+        {
+            var missingWinningHandsException = new MissingWinningHandsException();
+            ValidateException(missingWinningHandsException, "Winning cards list is null or empty.");
+        }
+    }
+}
---
+++ Poker/Exceptions/MissingWinningHandsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poker.Exceptions
+{
+    public sealed class MissingWinningHandsException : Exception
+    {
+        public MissingWinningHandsException()
+            : base($"Winning cards list is null or empty.")
+        {
+        }
+    }
+}
