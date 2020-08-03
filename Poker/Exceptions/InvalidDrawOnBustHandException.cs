d6ac0afb115e6b34417f4ce3db51ebb8d2b99645
Victor De Velasco <victor91dv@hotmail.com>
2020-08-02 19:13:04 -0700

    Added new blackjack exception.

---
+++ Poker/Exceptions/InvalidDrawOnBustHandException.cs
@@ -0,0 +1,14 @@
+﻿using Poker.Extensions;
+using Poker.Models;
+using System;
+
+namespace Poker.Exceptions
+{
+    public sealed class InvalidDrawOnBustHandException : Exception
+    {
+        public InvalidDrawOnBustHandException(Hand hand)
+            : base($"Cannot add a new card on a bust hand. [Hand: {hand.Stringify()}]")
+        {
+        }
+    }
+}
